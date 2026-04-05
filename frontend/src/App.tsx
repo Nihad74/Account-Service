333088837e2f6042700a99f0b50a627f4bdbe08d
Nihad Uddin <nihad.uddin@myyahoo.com>
2026-04-05 23:51:02 +0200

    login page

---
+++ frontend/src/App.tsx
@@ -0,0 +1,39 @@
+import './App.css'
+import {useState, type SubmitEvent} from "react";
+
+function App() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  function handleSubmit(e: SubmitEvent<HTMLFormElement>){
+      e.preventDefault()
+
+      console.log("Email: ", email)
+      console.log("Password: ", password)
+
+  }
+
+  return (
+      <main className={"login-page"}>
+        <div className={"login-card"}>
+          <h1>LOG IN TO YOUR ACCOUNT</h1>
+            <form className={"login-form"} onSubmit={handleSubmit}>
+                <div className={"input-group"}>
+                    <label>Email</label>
+                    <input type="email" placeholder={"Enter your email"} value={email}
+                    onChange = {(e) => setEmail(e.target.value)}/>
+                </div>
+                <div className={"input-group"}>
+                    <label>Password</label>
+                    <input type="password" placeholder={"********"} value={password}
+                    onChange = {(e) => setPassword(e.target.value)}/>
+                </div>
+            <button className="login-button" type={"submit"}>LOG IN</button>
+            </form>
+            <a className={"forgot-password"} href={"/forgot-password"}>Forgot your Password?</a>
+        </div>
+      </main>
+  )
+}
+
+export default App
