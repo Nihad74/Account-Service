75ebeea0e752836f7c781e06723bb09812b37911
Nihad Uddin <nihad.uddin@myyahoo.com>
2026-04-05 23:51:02 +0200

    login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import {useState, type SubmitEvent} from "react";
 function App() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  //const [error, setError] = useState("")
+  //const [isSubmitting, setIsSubmitting] = useState(false)
 
   function handleSubmit(e: SubmitEvent<HTMLFormElement>){
       e.preventDefault()
@@ -19,13 +21,13 @@ function App() {
           <h1>LOG IN TO YOUR ACCOUNT</h1>
             <form className={"login-form"} onSubmit={handleSubmit}>
                 <div className={"input-group"}>
-                    <label>Email</label>
-                    <input type="email" placeholder={"Enter your email"} value={email}
+                    <label htmlFor={"email"}>Email</label>
+                    <input type="email" placeholder={"Enter your email"} value={email} id={"email"}
                     onChange = {(e) => setEmail(e.target.value)}/>
                 </div>
                 <div className={"input-group"}>
-                    <label>Password</label>
-                    <input type="password" placeholder={"********"} value={password}
+                    <label htmlFor={"password"}>Password</label>
+                    <input type="password" placeholder={"********"} value={password} id={"password"}
                     onChange = {(e) => setPassword(e.target.value)}/>
                 </div>
             <button className="login-button" type={"submit"}>LOG IN</button>
